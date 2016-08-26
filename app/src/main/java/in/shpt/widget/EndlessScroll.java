da87dce51133ff6b897e4f1fed34b7d29ae84414
poovarasan <poosan9@gmail.com>
2016-08-26 10:53:20 +0530

    Latest SHPT Commits

--- app/src/main/java/in/shpt/widget/EndlessScroll.java
+++ app/src/main/java/in/shpt/widget/EndlessScroll.java
@@ -11,15 +11,15 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 public abstract class EndlessScroll extends RecyclerView.OnScrollListener {
     // The minimum amount of items to have below your current scroll position
     // before loading more.
-    private int visibleThreshold = 15;
+    private int visibleThreshold = 5;
     // The current offset index of data you have loaded
-    private int currentPage = 1;
+    private int currentPage = 0;
     // The total number of items in the dataset after the last load
     private int previousTotalItemCount = 0;
     // True if we are still waiting for the last set of data to load.
     private boolean loading = true;
     // Sets the starting page index
-    private int startingPageIndex = 1;
+    private int startingPageIndex = 0;
 
     RecyclerView.LayoutManager mLayoutManager;
 
